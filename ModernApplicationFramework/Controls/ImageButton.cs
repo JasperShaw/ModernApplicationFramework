8a5e59712295ddf238aefbd00d090edc230999e9
AnakinSklavenwalker <anakin@gmx-topmail.de>
2015-07-18 23:30:40 +0200

    Added ImageButton

---
+++ ModernApplicationFramework/Controls/ImageButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace ModernApplicationFramework.Controls
+{
+    public class ImageButton : Button
+    {
+        public static readonly DependencyProperty HoverImageProperty = DependencyProperty.Register(
+            "HoverImage", typeof (ImageSource), typeof (ImageButton), new PropertyMetadata(default(ImageSource)));
+
+        public static readonly DependencyProperty NormalImageProperty = DependencyProperty.Register(
+            "NormalImage", typeof (ImageSource), typeof (ImageButton), new PropertyMetadata(default(ImageSource)));
+
+        public static readonly DependencyProperty PressedImageProperty = DependencyProperty.Register(
+            "PressedImage", typeof (ImageSource), typeof (ImageButton), new PropertyMetadata(default(ImageSource)));
+
+        static ImageButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof (ImageButton),
+                new FrameworkPropertyMetadata(typeof (ImageButton)));
+        }
+
+        public ImageSource HoverImage
+        {
+            get { return (ImageSource) GetValue(HoverImageProperty); }
+            set { SetValue(HoverImageProperty, value); }
+        }
+
+        public ImageSource NormalImage
+        {
+            get { return (ImageSource) GetValue(NormalImageProperty); }
+            set { SetValue(NormalImageProperty, value); }
+        }
+
+        public ImageSource PressedImage
+        {
+            get { return (ImageSource) GetValue(PressedImageProperty); }
+            set { SetValue(PressedImageProperty, value); }
+        }
+    }
+}
