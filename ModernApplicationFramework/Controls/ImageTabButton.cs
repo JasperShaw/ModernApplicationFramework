2ada77a7bdb5023deec89774ec60231d1aaa9dad
AnakinSklavenwalker <anakin@gmx-topmail.de>
2015-11-12 18:24:28 +0100

    added new control (image tab) added contentpresenter

---
+++ ModernApplicationFramework/Controls/ImageTabButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace ModernApplicationFramework.Controls
+{
+    public class ImageTabButton : ImageRadioButton
+    {
+        static ImageTabButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ImageTabButton),
+                new FrameworkPropertyMetadata(typeof(ImageTabButton)));
+        }
+    }
+}
