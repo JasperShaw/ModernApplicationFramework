69c523d9d50981345f37a93d7849daa0ac7a5b5e
AnakinSklavenwalker <anakin@gmx-topmail.de>
2016-03-29 16:04:46 +0200

    forgotten files

---
+++ CaliBurnTest/Views/AppView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace CaliBurnTest.Views
+{
+    /// <summary>
+    /// Interaktionslogik für AppView.xaml
+    /// </summary>
+    public partial class AppView
+    {
+        public AppView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ModernApplicationFramework/MVVM/Views/DockingMainWindowView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ModernApplicationFramework.MVVM.Views
+{
+    /// <summary>
+    /// Interaktionslogik für App1View.xaml
+    /// </summary>
+    public partial class DockingMainWindowView
+    {
+        public DockingMainWindowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
