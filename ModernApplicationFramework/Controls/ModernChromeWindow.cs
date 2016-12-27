8432855d1ab4a8d0501e8f897d930286985d7e93
Anakin <Anakin@DESKTOP-H8OMESS>
2016-12-27 17:18:16 +0100

    refactor

--- ModernApplicationFramework/Controls/ModernChromeWindow.cs
+++ ModernApplicationFramework/Controls/ModernChromeWindow.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics.CodeAnalysis;
-using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
