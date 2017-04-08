54a1c524bf63a4262de88a1df8c5680de446ee68
AnakinSklavenwalker <Anakin@gmx-topmail.de>
2017-04-08 20:07:17 +0200

    added way to select a specific context menu for document tab items

---
+++ ModernApplicationFramework/Core/Converters/ModelIsAnchorableToBoolConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+using ModernApplicationFramework.Docking;
+using ModernApplicationFramework.Docking.Layout;
+
+namespace ModernApplicationFramework.Core.Converters
+{
+    public class DocumentTabModelToContextMenuConverter : ValueConverter<object, ContextMenu>
+    {
+        protected override ContextMenu Convert(object value, object parameter, CultureInfo culture)
+        {
+            if (value is LayoutAnchorable)
+                return DockingManager.Instace.AnchorableContextMenu;
+            return DockingManager.Instace.DocumentContextMenu;
+        }
+    }
+}
