007bfe0ca0a3e72d0b111baf2ac8c8928b7177c5
AnakinSklavenwalker <Anakin@gmx-topmail.de>
2017-04-13 18:24:48 +0200

    applied previous code to context menu

--- ModernApplicationFramework/Basics/Creators/ContextMenuCreator.cs
+++ ModernApplicationFramework/Basics/Creators/ContextMenuCreator.cs
@@ -6,6 +6,8 @@ using Caliburn.Micro;
 using ModernApplicationFramework.Basics.Definitions.Command;
 using ModernApplicationFramework.Basics.Definitions.CommandBar;
 using ModernApplicationFramework.Basics.Definitions.ContextMenu;
+using ModernApplicationFramework.Basics.Definitions.Menu;
+using ModernApplicationFramework.Basics.Definitions.Menu.MenuItems;
 using ModernApplicationFramework.Controls;
 using ModernApplicationFramework.Interfaces;
 using ContextMenu = ModernApplicationFramework.Controls.ContextMenu;
@@ -39,17 +41,18 @@ namespace ModernApplicationFramework.Basics.Creators
                     .Where(x => !host.ExcludedContextMenuElementDefinitions.Contains(x))
                     .OrderBy(x => x.SortOrder);
 
-
+                var firstItem = false;
                 if (i > 0 && i <= groups.Count - 1 && menuItems.Any())
                 {
                     if (menuItems.Any(menuItemDefinition => menuItemDefinition.IsVisible))
                     {
                         var separator = new MenuItem(CommandBarSeparatorDefinition.MenuSeparatorDefinition);
                         contextMenu.Items.Add(separator);
+                        firstItem = true;
                     }
                 }
-            
 
+                uint newSortOrder = 0;
                 foreach (var menuItemDefinition in menuItems)
                 {
                     MenuItem menuItemControl;
@@ -57,8 +60,11 @@ namespace ModernApplicationFramework.Basics.Creators
                         menuItemControl = new DummyListMenuItem(menuItemDefinition, contextMenu);
                     else
                         menuItemControl = new MenuItem(menuItemDefinition);
+                    menuItemDefinition.PrecededBySeparator = firstItem;
+                    firstItem = false;
                     CreateContextMenuTree(menuItemDefinition, menuItemControl);
                     contextMenu.Items.Add(menuItemControl);
+                    menuItemDefinition.SortOrder = newSortOrder++;
                 }
             }
         }
@@ -85,9 +91,36 @@ namespace ModernApplicationFramework.Basics.Creators
                     list.AddRange(menuItems);
                 }
             }
-            else
+            else if (contextMenuDefinition is MenuDefinition || contextMenuDefinition is MenuItemDefinition)
             {
-                
+                var groups = model.MenuItemGroupDefinitions.Where(x => x.Parent == contextMenuDefinition)
+                    .OrderBy(x => x.SortOrder)
+                    .ToList();
+
+                uint newSortOrder = 0;  //As Menus are created each click we need to to this also in this methods
+
+                for (var i = 0; i < groups.Count; i++)
+                {
+                    var group = groups[i];
+                    var menuItems = model.MenuItemDefinitions.Where(x => x.Group == group)
+                        .OrderBy(x => x.SortOrder);
+
+                    bool firstItem = false; //As Menus are created each click we need to to this also in this methods
+                    if (i > 0 && i <= groups.Count - 1 && menuItems.Any())
+                        if (menuItems.Any(menuItemDefinition => menuItemDefinition.IsVisible))
+                        {
+                            list.Add(CommandBarSeparatorDefinition.MenuSeparatorDefinition);
+                            firstItem = true;
+                        }
+
+                    foreach (var itemDefinition in menuItems)
+                    {
+                        itemDefinition.PrecededBySeparator = firstItem;
+                        list.Add(itemDefinition);
+                        itemDefinition.SortOrder = newSortOrder++;
+                        firstItem = false;
+                    }
+                }
             }
             return list;
         }
