10fb46894d221d879d864ba2b84e2c48b688453e
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2017-07-13 16:31:12 +0200

    Started LayoutManager

---
+++ ModernApplicationFramework.Extended/Commands/SaveCurrentLayoutCommandDefinition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows.Input;
+using ModernApplicationFramework.Basics;
+using ModernApplicationFramework.Basics.Definitions.Command;
+using ModernApplicationFramework.Extended.Core.LayoutUtilities;
+
+namespace ModernApplicationFramework.Extended.Commands
+{
+    [Export(typeof(DefinitionBase))]
+    [Export(typeof(SaveCurrentLayoutCommandDefinition))]
+    public sealed class SaveCurrentLayoutCommandDefinition : CommandDefinition
+    {
+        private readonly ILayoutManager _layoutManager;
+        public override string Name => Text;
+        public override string Text => "Save Window Layout";
+        public override string ToolTip => null;
+        public override Uri IconSource => null;
+        public override string IconId => null;
+        public override CommandCategory Category => CommandCategories.WindowCommandCategory;
+
+        public override ICommand Command { get; }
+
+        [ImportingConstructor]
+        public SaveCurrentLayoutCommandDefinition(ILayoutManager layoutManager)
+        {
+            _layoutManager = layoutManager;
+        }
+    }
+}
---
+++ ModernApplicationFramework.Extended/Core/LayoutUtilities/LayoutManager.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.Composition;
+using ModernApplicationFramework.Extended.Interfaces;
+
+namespace ModernApplicationFramework.Extended.Core.LayoutUtilities
+{
+    [Export(typeof(ILayoutManager))]
+    public class LayoutManager : ILayoutManager
+    {
+        private readonly ILayoutItemStatePersister _layoutItemStatePersister;
+
+        [ImportingConstructor]
+        public LayoutManager(ILayoutItemStatePersister layoutItemStatePersister)
+        {
+            _layoutItemStatePersister = layoutItemStatePersister;
+        }
+
+        public void Load()
+        {       
+        }
+
+        public void Save()
+        {
+        }
+
+        public void Apply()
+        {
+        }
+    }
+
+    public interface ILayoutManager
+    {
+        void Load();
+
+        void Save();
+
+        void Apply();
+    }
+}
