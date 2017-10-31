4970fc25fb0e95bde7404abd26f413792fc8757c
Nesterenko Konstantin <nestquik@gmail.com>
2017-10-31 10:48:03 +0200

    Various improvements

--- samples/ModernApplicationFramework.Extended.DemoMin/DemoBootstrapper.cs
+++ samples/ModernApplicationFramework.Extended.DemoMin/DemoBootstrapper.cs
@@ -2,8 +2,13 @@
 
 namespace ModernApplicationFramework.Extended.DemoMin
 {
-    public class DemoBootstrapper : ExtendedBootstrapper
+    public sealed class DemoBootstrapper : ExtendedBootstrapper
     {
         protected override IExtendedEnvironmentVariables EnvironmentVariables => new DemoEnvironmentVariables();
+
+        public DemoBootstrapper()
+        {
+            Initialize();
+        }
     }
 }
--- src/ModernApplicationFramework.Extended/ExtendedBootstrapper.cs
+++ src/ModernApplicationFramework.Extended/ExtendedBootstrapper.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
-using System.Linq;
-using System.Reflection;
 using System.Windows;
 using Caliburn.Micro;
 using ModernApplicationFramework.Basics.Services;
@@ -13,13 +11,11 @@ using ModernApplicationFramework.Utilities.Interfaces;
 
 namespace ModernApplicationFramework.Extended
 {
-    public class ExtendedBootstrapper : Bootstrapper
+    public abstract class ExtendedBootstrapper : Bootstrapper
     {
         protected virtual bool UseSettingsManager => true;
 
         protected new virtual IExtendedEnvironmentVariables EnvironmentVariables => new FallbackExtendedEnvironmentVariables();
-
-        internal IList<Assembly> PriorityAssemblies => _priorityAssemblies;
 
         protected override void BindServices(CompositionBatch batch)
         {
@@ -28,18 +24,7 @@ namespace ModernApplicationFramework.Extended
             base.BindServices(batch);
             batch.AddExportedValue(this);
         }
-
-        protected override object GetInstance(Type serviceType, string key)
-        {
-            var contract = string.IsNullOrEmpty(key) ? AttributedModelServices.GetContractName(serviceType) : key;
-            var exports = Container.GetExportedValues<object>(contract);
-
-            var enumerable = exports as object[] ?? exports.ToArray();
-            if (enumerable.Any())
-                return enumerable.First();
-            return null;
-        }
-
+        
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             base.OnStartup(sender, e);
@@ -53,14 +38,6 @@ namespace ModernApplicationFramework.Extended
         {
             IoC.Get<IApplicationEnvironment>().PrepareClose();
             base.OnExit(sender, e);
-        }
-
-        protected override IEnumerable<Assembly> SelectAssemblies()
-        {
-            return new[]
-            {
-                Assembly.GetEntryAssembly()
-            };
         }
     }
 }
