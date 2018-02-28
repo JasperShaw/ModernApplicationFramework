715f79327948be7a6effb16d02c4bfac30b23802
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2018-02-28 19:36:33 +0100

    removed misterious Result code stuff

--- src/ModernApplicationFramework.Extended.Modules/InspectorTool/Inspectors/BitmapSourceEditorViewModel.cs
+++ src/ModernApplicationFramework.Extended.Modules/InspectorTool/Inspectors/BitmapSourceEditorViewModel.cs
@@ -1,29 +1,26 @@
-﻿using System.Collections.Generic;
-using System.Windows.Media.Imaging;
-using Caliburn.Micro;
-using Microsoft.Win32;
-using ModernApplicationFramework.Extended.ResultDialogs;
+﻿using System.Windows.Media.Imaging;
 
 namespace ModernApplicationFramework.Extended.Modules.InspectorTool.Inspectors
 {
     public class BitmapSourceEditorViewModel : EditorBase<BitmapSource>, ILabelledInspector
     {
-        public IEnumerable<IResult> Choose()
-        {
-            var fileDialog = new OpenFileDialog();
-            yield return Show.CommonDialog(fileDialog);
+        //TODO: Decide what to do with this...
+        //public IEnumerable<IResult> Choose()
+        //{
+        //    var fileDialog = new OpenFileDialog();
+        //    yield return Show.CommonDialog(fileDialog);
 
-            using (var stream = fileDialog.OpenFile())
-            {
-                var result = new BitmapImage();
-                result.BeginInit();
-                result.CacheOption = BitmapCacheOption.OnLoad;
-                result.StreamSource = stream;
-                result.EndInit();
-                result.Freeze();
+        //    using (var stream = fileDialog.OpenFile())
+        //    {
+        //        var result = new BitmapImage();
+        //        result.BeginInit();
+        //        result.CacheOption = BitmapCacheOption.OnLoad;
+        //        result.StreamSource = stream;
+        //        result.EndInit();
+        //        result.Freeze();
 
-                Value = result;
-            }
-        }
+        //        Value = result;
+        //    }
+        //}
     }
 }
