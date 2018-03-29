9672fbf803faa44cfcc1f9b2c7db1bd36a49c6ec
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2018-03-29 13:52:36 +0200

    added ColorToBrushConverter

---
+++ src/ModernApplicationFramework.Utilities/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using System.Windows.Media;
+
+namespace ModernApplicationFramework.Utilities.Converters
+{
+    public class ColorToBrushConverter : ValueConverter<Color, Brush>
+    {
+        protected override Brush Convert(Color value, object parameter, CultureInfo culture)
+        {
+            return new SolidColorBrush(value);
+        }
+    }
+}
