32f935b77447398a1770ca73eba04b648707b6da
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2018-05-29 21:38:36 +0200

    selection matches VS

--- src/ModernApplicationFramework.Modules.Toolbox/Controls/ToolboxTreeViewItem.cs
+++ src/ModernApplicationFramework.Modules.Toolbox/Controls/ToolboxTreeViewItem.cs
@@ -1,12 +1,27 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Threading;
 using ModernApplicationFramework.Utilities;
 
 namespace ModernApplicationFramework.Modules.Toolbox.Controls
 {
+    [TemplatePart(Name = "PART_EmptyMessage", Type = typeof(UIElement))]
     internal class ToolboxTreeViewItem : TreeViewItem
     {
+        private bool _doubleClicked;
+        private DispatcherTimer _timer;
+        private UIElement _emptyMessageHolder;
+
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _emptyMessageHolder = GetTemplateChild("PART_EmptyMessage") as UIElement;
+        }
+
         protected override void OnKeyDown(KeyEventArgs e)
         {
             base.OnKeyDown(e);
@@ -39,6 +54,52 @@ namespace ModernApplicationFramework.Modules.Toolbox.Controls
                     e.Handled = true;
                     break;
             }
+        }
+
+        protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            if (_doubleClicked || _emptyMessageHolder != null && _emptyMessageHolder.IsMouseOver)
+                return;
+            StartTimer();
+            if (DataContext is ToolboxItemCategory)
+                IsExpanded = !IsExpanded;
+            e.Handled = true;
+            base.OnMouseLeftButtonUp(e);
+        }
+
+        protected override void OnMouseDoubleClick(MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            base.OnMouseDoubleClick(e);
+        }
+
+        private void StartTimer()
+        {
+            if (_timer != null)
+                return;
+            _doubleClicked = true;
+            _timer = new DispatcherTimer(TimeSpan.FromMilliseconds(GetDoubleClickTime()), DispatcherPriority.Input,
+                (sender, e) => StopTimer(), Dispatcher);
+            _timer.Start();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern uint GetDoubleClickTime();
+
+        private void StopTimer()
+        {
+            _doubleClicked = false;
+            if (_timer == null)
+                return;
+            _timer.Stop();
+            _timer = null;
+        }
+
+        protected override void OnPreviewMouseRightButtonDown(MouseButtonEventArgs e)
+        {
+            if (!IsKeyboardFocusWithin)
+                Focus();
+            base.OnPreviewMouseRightButtonDown(e);
         }
 
         protected override DependencyObject GetContainerForItemOverride()
