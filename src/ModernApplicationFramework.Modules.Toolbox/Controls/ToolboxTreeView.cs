eb233805aea56a80dd850100dd27981d72d178b7
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2018-05-29 18:25:43 +0200

    move to own file

--- src/ModernApplicationFramework.Modules.Toolbox/Controls/ToolboxTreeView.cs
+++ src/ModernApplicationFramework.Modules.Toolbox/Controls/ToolboxTreeView.cs
@@ -1,78 +1,10 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using ModernApplicationFramework.Utilities;
 
 namespace ModernApplicationFramework.Modules.Toolbox.Controls
 {
     internal class ToolboxTreeView : TreeView
     {
-        protected override DependencyObject GetContainerForItemOverride()
-        {
-            return new ToolboxTreeViewItem();
-        }
-
-        protected override bool IsItemItsOwnContainerOverride(object item)
-        {
-            return item is ToolboxTreeViewItem;
-        }
-    }
-
-    internal class ToolboxTreeViewItem : TreeViewItem
-    {
-        protected override void OnKeyDown(KeyEventArgs e)
-        {
-            base.OnKeyDown(e);
-            if (e.Handled)
-                return;
-            switch (e.Key)
-            {
-                case Key.Right:
-                case Key.Left:
-                    if (!IsLogicalLeft(e.Key))
-                    {
-                        if (DataContext is IToolboxItem)
-                            break;
-                        if (!IsExpanded)
-                        {
-                            IsExpanded = true;
-                            e.Handled = true;
-                            break;
-                        }
-                        HandleLogicalRight();
-                        e.Handled = true;
-                        break;
-                    }
-                    if (DataContext is IToolboxItem)
-                        break;
-                    if (IsExpanded)
-                        IsExpanded = false;
-                    else
-                        HandleLogicalLeft();
-                    e.Handled = true;
-                    break;
-            }
-        }
-
-        private bool IsLogicalLeft(Key key)
-        {
-            if (FlowDirection != FlowDirection.LeftToRight)
-                return key == Key.Right;
-            return key == Key.Left;
-        }
-
-        private void HandleLogicalRight()
-        {
-            MoveFocus(new TraversalRequest(FocusNavigationDirection.Down));
-        }
-
-        private void HandleLogicalLeft()
-        {
-            var parent = this.FindAncestorOrSelf<TreeView>();
-            ((UIElement)parent?.ItemContainerGenerator.ContainerFromItem(parent))?.Focus();
-        }
-
-
         protected override DependencyObject GetContainerForItemOverride()
         {
             return new ToolboxTreeViewItem();
