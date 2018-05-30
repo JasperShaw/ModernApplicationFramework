3e49e55642244f1894f7ffe79197c35e2e3fade9
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2018-05-30 10:39:50 +0200

    include ignored file

---
+++ src/ModernApplicationFramework.EditorBase/Interfaces/Packages/IMruFilePackage.cs
@@ -0,0 +1,9 @@
+﻿using ModernApplicationFramework.EditorBase.FileSupport;
+
+namespace ModernApplicationFramework.EditorBase.Interfaces.Packages
+{
+    public interface IMruFilePackage
+    {
+        FileSystemMruManager Manager { get; }
+    }
+}
