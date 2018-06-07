696519590f1311725114fe8f4f33ce237938bf53
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2018-06-07 11:44:40 +0200

    prepare delete hotkey

--- src/ModernApplicationFramework.Modules.Toolbox/Controls/ToolboxTreeView.cs
+++ src/ModernApplicationFramework.Modules.Toolbox/Controls/ToolboxTreeView.cs
@@ -1,6 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
+using ModernApplicationFramework.Input.Command;
 using ModernApplicationFramework.Modules.Toolbox.Interfaces;
 using ModernApplicationFramework.Utilities;
 
@@ -8,6 +11,37 @@ namespace ModernApplicationFramework.Modules.Toolbox.Controls
 {
     internal class ToolboxTreeView : TreeView
     {
+        public ToolboxTreeView()
+        {
+            RegisterCommandHandlers(typeof(ToolboxTreeView));
+        }
+
+        private static void RegisterCommandHandlers(Type controlType)
+        {
+            CommandHelpers.RegisterCommandHandler(controlType, EditingCommands.Delete, OnDelete, CanDelete, Key.Delete);
+        }
+
+        private static void CanDelete(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (sender is ToolboxTreeView tv)
+                e.CanExecute = tv.CanDelete();                        
+        }
+
+        private bool CanDelete()
+        {
+            return SelectedItem is IToolboxNode;
+        }
+
+        private static void OnDelete(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (sender is ToolboxTreeView tv)
+                tv.OnDelete();
+        }
+
+        private void OnDelete()
+        {
+        }
+
         protected override DependencyObject GetContainerForItemOverride()
         {
             return new ToolboxTreeViewItem();
