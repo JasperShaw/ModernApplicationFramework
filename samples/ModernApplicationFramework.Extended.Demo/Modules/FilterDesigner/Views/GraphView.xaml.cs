7d10e9f73122a9a4f394a0009d9d215e990b8d66
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2018-06-08 13:21:11 +0200

    Filter sample supports delete

--- samples/ModernApplicationFramework.Extended.Demo/Modules/FilterDesigner/Views/GraphView.xaml.cs
+++ samples/ModernApplicationFramework.Extended.Demo/Modules/FilterDesigner/Views/GraphView.xaml.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
 using ModernApplicationFramework.Extended.Demo.GraphDesigner.Controls;
 using ModernApplicationFramework.Extended.Demo.Modules.FilterDesigner.ViewModels;
-using ModernApplicationFramework.Modules.Toolbox;
+using ModernApplicationFramework.Input.Command;
 
 namespace ModernApplicationFramework.Extended.Demo.Modules.FilterDesigner.Views
 {
@@ -12,11 +12,22 @@ namespace ModernApplicationFramework.Extended.Demo.Modules.FilterDesigner.Views
     {
         private Point _originalContentMouseDownPoint;
 
-        private GraphViewModel ViewModel => (GraphViewModel)DataContext;
+        private GraphViewModel ViewModel => (GraphViewModel) DataContext;
 
         public GraphView()
         {
             InitializeComponent();
+            CommandHelpers.RegisterCommandHandler(GetType(), EditingCommands.Delete, OnDelete, CanDelete);
+        }
+
+        protected virtual bool CanDelete()
+        {
+            return DataContext is GraphViewModel;
+        }
+
+        protected virtual void OnDelete()
+        {
+            ((GraphViewModel) DataContext).DeleteSelectedElements();
         }
 
         protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
@@ -26,26 +37,40 @@ namespace ModernApplicationFramework.Extended.Demo.Modules.FilterDesigner.Views
             base.OnPreviewMouseDown(e);
         }
 
-        protected override void OnKeyDown(KeyEventArgs e)
+        private void CanDelete(object sender, CanExecuteRoutedEventArgs e)
         {
-            if (e.Key == Key.Delete)
-                ((GraphViewModel)DataContext).DeleteSelectedElements();
-            base.OnKeyDown(e);
+            if (!(sender is GraphView graphView))
+                e.CanExecute = false;
+            else
+                e.CanExecute = graphView.CanDelete();
         }
 
-        private void OnGraphControlRightMouseButtonDown(object sender, MouseButtonEventArgs e)
+        private void OnDelete(object sender, ExecutedRoutedEventArgs e)
         {
-            _originalContentMouseDownPoint = e.GetPosition(GraphControl);
-            GraphControl.CaptureMouse();
-            Mouse.OverrideCursor = Cursors.ScrollAll;
-            e.Handled = true;
+            ((GraphView) sender)?.OnDelete();
         }
 
-        private void OnGraphControlRightMouseButtonUp(object sender, MouseButtonEventArgs e)
+        private void OnGraphControlConnectionDragCompleted(object sender, ConnectionDragCompletedEventArgs e)
         {
-            Mouse.OverrideCursor = null;
-            GraphControl.ReleaseMouseCapture();
-            e.Handled = true;
+            var currentDragPoint = Mouse.GetPosition(GraphControl);
+            var sourceConnector = (ConnectorViewModel) e.SourceConnector.DataContext;
+            var newConnection = (ConnectionViewModel) e.Connection;
+            ViewModel.OnConnectionDragCompleted(currentDragPoint, newConnection, sourceConnector);
+        }
+
+        private void OnGraphControlConnectionDragging(object sender, ConnectionDraggingEventArgs e)
+        {
+            var currentDragPoint = Mouse.GetPosition(GraphControl);
+            var connection = (ConnectionViewModel) e.Connection;
+            ViewModel.OnConnectionDragging(currentDragPoint, connection);
+        }
+
+        private void OnGraphControlConnectionDragStarted(object sender, ConnectionDragStartedEventArgs e)
+        {
+            var sourceConnector = (ConnectorViewModel) e.SourceConnector.DataContext;
+            var currentDragPoint = Mouse.GetPosition(GraphControl);
+            var connection = ViewModel.OnConnectionDragStarted(sourceConnector, currentDragPoint);
+            e.Connection = connection;
         }
 
         private void OnGraphControlMouseMove(object sender, MouseEventArgs e)
@@ -71,32 +96,24 @@ namespace ModernApplicationFramework.Extended.Demo.Modules.FilterDesigner.Views
             e.Handled = true;
         }
 
+        private void OnGraphControlRightMouseButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _originalContentMouseDownPoint = e.GetPosition(GraphControl);
+            GraphControl.CaptureMouse();
+            Mouse.OverrideCursor = Cursors.ScrollAll;
+            e.Handled = true;
+        }
+
+        private void OnGraphControlRightMouseButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Mouse.OverrideCursor = null;
+            GraphControl.ReleaseMouseCapture();
+            e.Handled = true;
+        }
+
         private void OnGraphControlSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             ViewModel.OnSelectionChanged();
-        }
-
-        private void OnGraphControlConnectionDragStarted(object sender, ConnectionDragStartedEventArgs e)
-        {
-            var sourceConnector = (ConnectorViewModel)e.SourceConnector.DataContext;
-            var currentDragPoint = Mouse.GetPosition(GraphControl);
-            var connection = ViewModel.OnConnectionDragStarted(sourceConnector, currentDragPoint);
-            e.Connection = connection;
-        }
-
-        private void OnGraphControlConnectionDragging(object sender, ConnectionDraggingEventArgs e)
-        {
-            var currentDragPoint = Mouse.GetPosition(GraphControl);
-            var connection = (ConnectionViewModel)e.Connection;
-            ViewModel.OnConnectionDragging(currentDragPoint, connection);
-        }
-
-        private void OnGraphControlConnectionDragCompleted(object sender, ConnectionDragCompletedEventArgs e)
-        {
-            var currentDragPoint = Mouse.GetPosition(GraphControl);
-            var sourceConnector = (ConnectorViewModel)e.SourceConnector.DataContext;
-            var newConnection = (ConnectionViewModel)e.Connection;
-            ViewModel.OnConnectionDragCompleted(currentDragPoint, newConnection, sourceConnector);
         }
     }
 }
