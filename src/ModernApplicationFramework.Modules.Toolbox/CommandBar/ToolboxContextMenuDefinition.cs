90ccc06c93ee6ef666ec6f23c3f8b596a661a3b4
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2018-06-09 15:57:41 +0200

    tweaked visibility settings a bit

--- src/ModernApplicationFramework.Modules.Toolbox/CommandBar/ToolboxContextMenuDefinition.cs
+++ src/ModernApplicationFramework.Modules.Toolbox/CommandBar/ToolboxContextMenuDefinition.cs
@@ -26,11 +26,11 @@ namespace ModernApplicationFramework.Modules.Toolbox.CommandBar
 
         [Export]
         public static CommandBarItemDefinition AddCategoryDefinition =
-            new CommandBarCommandItemDefinition<AddCategoryCommandDefinition>(new Guid("{AB8D308E-FD87-47B6-AD26-4FCB78CE9CEC}"), CategoryCommandGroup, 0, true, false, false, true);
+            new CommandBarCommandItemDefinition<AddCategoryCommandDefinition>(new Guid("{AB8D308E-FD87-47B6-AD26-4FCB78CE9CEC}"), CategoryCommandGroup, 0);
 
         [Export]
         public static CommandBarItemDefinition RemoveCategoryDefinition =
-            new CommandBarCommandItemDefinition<DeleteActiveToolbarCategoryCommandDefinition>(new Guid("{BF87A03B-EAFB-4778-8243-90BE338F67EA}"), CategoryCommandGroup, 1, true, false, false, true);
+            new CommandBarCommandItemDefinition<DeleteActiveToolbarCategoryCommandDefinition>(new Guid("{BF87A03B-EAFB-4778-8243-90BE338F67EA}"), CategoryCommandGroup, 1);
 
         [Export]
         public static CommandBarItemDefinition RenameCategoryDefinition =
