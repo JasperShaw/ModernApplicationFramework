4db6174cb49b95456b2f9f6bd5e471ef8c72cb9d
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2018-06-11 22:01:23 +0200

    added 4 way multi converter

--- src/ModernApplicationFramework.Utilities/Converters/MultiValueConverter.cs
+++ src/ModernApplicationFramework.Utilities/Converters/MultiValueConverter.cs
@@ -72,4 +72,41 @@ namespace ModernApplicationFramework.Utilities.Converters
             throw MakeConverterFunctionNotDefinedException();
         }
     }
+
+    public class MultiValueConverter<TSource1, TSource2, TSource3, TSource4, TTarget> : MultiValueConverterBase<TTarget>
+    {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!ValidateConvertParameters(values, targetType))
+                return default(TTarget);
+            return Convert(MultiValueHelper.CheckValue<TSource1>(values, 0), MultiValueHelper.CheckValue<TSource2>(values, 1), MultiValueHelper.CheckValue<TSource3>(values, 2), MultiValueHelper.CheckValue<TSource4>(values, 3), parameter, culture);
+        }
+
+        public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            ValidateConvertBackParameters(value, targetTypes);
+            MultiValueHelper.CheckType<TSource1>(targetTypes, 0);
+            MultiValueHelper.CheckType<TSource2>(targetTypes, 1);
+            MultiValueHelper.CheckType<TSource3>(targetTypes, 2);
+            MultiValueHelper.CheckType<TSource4>(targetTypes, 3);
+            ConvertBack((TTarget)value, out TSource1 obj1, out TSource2 obj2, out TSource3 obj3, out TSource4 obj4, parameter, culture);
+            return new object[]
+            {
+                obj1,
+                obj2,
+                obj3,
+                obj4
+            };
+        }
+
+        protected virtual TTarget Convert(TSource1 value1, TSource2 value2, TSource3 value3, TSource4 value4, object parameter, CultureInfo culture)
+        {
+            throw MakeConverterFunctionNotDefinedException();
+        }
+
+        protected virtual void ConvertBack(TTarget value, out TSource1 value1, out TSource2 value2, out TSource3 value3, out TSource4 value4, object parameter, CultureInfo culture)
+        {
+            throw MakeConverterFunctionNotDefinedException();
+        }
+    }
 }
