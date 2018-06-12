26a2de2f9d1b93ebfa2ffcbab589b5072d81effe
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2018-06-12 17:40:06 +0200

    default category always visible

--- src/ModernApplicationFramework.Modules.Toolbox/Converters/CategoryVisibilityConverter.cs
+++ src/ModernApplicationFramework.Modules.Toolbox/Converters/CategoryVisibilityConverter.cs
@@ -1,14 +1,16 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Windows;
+using ModernApplicationFramework.Modules.Toolbox.Items;
 using ModernApplicationFramework.Utilities.Converters;
 
 namespace ModernApplicationFramework.Modules.Toolbox.Converters
 {
-    internal class CategoryVisibilityConverter : MultiValueConverter<bool, bool, bool, bool, Visibility>
+    internal class CategoryVisibilityConverter : MultiValueConverter<bool, bool, bool, bool, Guid, Visibility>
     {
-        protected override Visibility Convert(bool value1, bool value2, bool value3, bool value4, object parameter, CultureInfo culture)
+        protected override Visibility Convert(bool value1, bool value2, bool value3, bool value4, Guid value5, object parameter, CultureInfo culture)
         {
-            if (value1 || value2 || value3 ||value4)
+            if (value1 || value2 || value3 ||value4 || value5.Equals(ToolboxItemCategory.DefaultCategoryId))
                 return Visibility.Visible;
             return Visibility.Collapsed;
         }
