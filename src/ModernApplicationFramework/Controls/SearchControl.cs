8abe4db1688e8da29d9f2bba829ce4dbea17a3c6
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2018-06-13 17:23:00 +0200

    started searchcontrol

---
+++ src/ModernApplicationFramework/Controls/SearchControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ModernApplicationFramework.Controls
+{
+    public class SearchControl : Control
+    {
+        static SearchControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SearchControl), new FrameworkPropertyMetadata(typeof(SearchControl)));
+        }
+    }
+}
