649cb6c3d9a38cc563483d46b08971642b226477
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2018-07-08 23:28:12 +0200

    added ballon sample

---
+++ samples/ModernApplicationFramework.Extended.Demo/Modules/BallonTooltip/BallonTooltipViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.Composition;
+using System.Media;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ModernApplicationFramework.Controls;
+using ModernApplicationFramework.Controls.Utilities;
+using ModernApplicationFramework.Extended.Layout;
+using ModernApplicationFramework.Extended.Utilities.PaneUtilities;
+using ModernApplicationFramework.Input.Command;
+
+namespace ModernApplicationFramework.Extended.Demo.Modules.BallonTooltip
+{
+    [Export(typeof(BallonTooltipViewModel))]
+    public class BallonTooltipViewModel : Tool
+    {
+        public override PaneLocation PreferredLocation => PaneLocation.Right;
+
+        public ICommand WarningCommand => new Command(ShowWarning, o => true);
+
+        public ICommand InfoCommand => new Command(ShowInfo, o => true);
+
+        public ICommand ErrorCommand => new Command(ShowError, o => true);
+
+        private void ShowInfo(object obj)
+        {
+            new BalloonTooltip(GetView() as Control, "Info", "Info" , BalloonType.Info, SystemSounds.Asterisk).Show();
+        }
+
+        private void ShowError(object obj)
+        {
+            new BalloonTooltip(GetView() as Control, "Error", "Error", BalloonType.Error, SystemSounds.Asterisk).Show();
+        }
+
+        private void ShowWarning(object o)
+        {
+            new BalloonTooltip(GetView() as Control, "Warning", "Warning", BalloonType.Warning, SystemSounds.Asterisk).Show();
+        }
+    }
+}
---
+++ samples/ModernApplicationFramework.Extended.Demo/Modules/BallonTooltip/Sample.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.Composition;
+using ModernApplicationFramework.Extended.Demo.Modules.SampleExplorer;
+using ModernApplicationFramework.Extended.Interfaces;
+
+namespace ModernApplicationFramework.Extended.Demo.Modules.BallonTooltip
+{
+    [Export(typeof(ISample))]
+    public class Sample : ISample
+    {
+        public string Name => "Ballon Tooltip";
+
+        public void Activate(IDockingHostViewModel shell)
+        {
+            shell?.ShowTool<BallonTooltipViewModel>();
+        }
+    }
+}
