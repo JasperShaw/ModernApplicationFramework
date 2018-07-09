f3cd7d11089ff297e131cce495b35eafda231a20
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2018-07-09 13:18:39 +0200

    icon is not focusable

--- src/ModernApplicationFramework.Modules.Toolbox/ToolboxView.xaml.cs
+++ src/ModernApplicationFramework.Modules.Toolbox/ToolboxView.xaml.cs
@@ -1,15 +1,10 @@
-﻿using ModernApplicationFramework.Controls.SearchControl;
-
-namespace ModernApplicationFramework.Modules.Toolbox
+﻿namespace ModernApplicationFramework.Modules.Toolbox
 {
     public partial class ToolboxView
     {
         public ToolboxView()
         {
             InitializeComponent();
-            //var d = new SearchControlDataSource {SearchSettings = {SearchWatermark = "Test"}};
-
-            //SearchControl.DataContext = d;
         }
     }
 }
